720dd489b60d7b90b2847490141c6ba1c3d7d380
sscnt <pixeltc@gmail.com>
2014-01-18 20:24:21 +0900

    Initial Commit

---
+++ HDRtest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HDRtest
+//
+//  Created by SSC on 2014/01/18.
+//  Copyright (c) 2014年 SSC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HDRtest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HDRtest
+//
+//  Created by SSC on 2014/01/18.
+//  Copyright (c) 2014年 SSC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
